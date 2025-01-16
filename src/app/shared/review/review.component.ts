6dcbbb384ea9a8d8a21070fc08312d82731cf9cb
mahima seenivasan <mahilaksh2k001@gmail.com>
2025-01-16 11:41:04 +0530

    review finalM

--- src/app/shared/review/review.component.ts
+++ src/app/shared/review/review.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
-  styleUrl: './review.component.css'
+  styleUrls: ['./review.component.css']
 })
 export class ReviewComponent {
+  rating: number = 0;  // Initialize the rating to 0
 
+  // Method to set the rating when a star is clicked
+  setRating(starValue: number): void {
+    this.rating = starValue;  // Update the rating value
+  }
 }
