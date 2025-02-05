8611c4e791c89def66465f4de7c13840f1910ebf
mahima seenivasan <mahilaksh2k001@gmail.com>
2025-02-05 16:41:53 +0530

    no admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,8 +25,7 @@ import { TopnavComponent } from './pages/topnav/topnav.component';
 import { FAQComponent } from './miscellaneous/faq/faq.component';
 import { PrivacypolicyComponent } from './miscellaneous/privacypolicy/privacypolicy.component';
 import { TermsofuseComponent } from './miscellaneous/termsofuse/termsofuse.component';
-import { AdminpageComponent } from './admin/adminpage/adminpage.component';
-import { ChatbotComponent } from './ai/chatbot/chatbot.component';
+
 import { HomeComponent } from './layout/home/home.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 
@@ -60,21 +59,12 @@ const routes: Routes = [
   {path:"store", component: StoreComponent},
   { path: "d", component: DummyComponent },
   { path: "dd", component: Dummy2Component },
-  { path: "admin", component: AdminpageComponent },
-  { path: "chat", component:ChatbotComponent },
+ 
 //store
   { path: 'home', component: HomeComponent },
   { path: 'checkout', component: CheckoutComponent },
     // Define routes for each table
-    { path: "table1", component: AdminpageComponent, data: { tableName: 'contact' } },
-    { path:"table2", component: AdminpageComponent, data: { tableName: 'feedback' } },
-    { path: "table3", component: AdminpageComponent, data: { tableName: 'get_quote' } },
-    { path: "table4", component: AdminpageComponent, data: { tableName: 'news_letter' } },
-    { path: "table5", component: AdminpageComponent, data: { tableName: 'users' } },
-    // Default route if no table is matched
-    { path: '', redirectTo: '/table1', pathMatch: 'full' },
-    // Wildcard for handling 404 errors
-    { path: '**', redirectTo: '/table1' }
+   
   ];
 
 
